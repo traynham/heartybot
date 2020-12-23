14bfc1a8ec04b913611f9e4c8ed1fca7150d51ce
traynham <jesse@comm-unity.net>
2020-12-23 21:49:15 +0000

    Moved database.sqlite3 to private folder.

--- util/sequelize.js
+++ util/sequelize.js
@@ -2,7 +2,8 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize('database', '', '', {
   dialect: 'sqlite',
-  storage: './data/database.sqlite3',
+  //storage: './data/database.sqlite3',
+  storage: './data/private/database.sqlite3',
   "logging": false
 })
 
