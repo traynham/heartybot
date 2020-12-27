99580614132fd7a4dea9eff8c0fd260a8f03d27f
traynham <jesse@comm-unity.net>
2020-12-27 01:39:27 +0000

    Refactored actions, updated show_boss to allow for filters in any args position.

--- discord/commands_general/commands/boss.js
+++ discord/commands_general/commands/boss.js
@@ -12,8 +12,6 @@
  * @name boss
  */
 
-
-//const dateFormat = require('dateformat')
 const Discord = require('discord.js');
 const bosses = require('@models_lowdb/bosses.js').bosses()
 const {detect, discord, help} = require(`@core`)
@@ -25,63 +23,31 @@ module.exports = {
 	name: 'boss',
 	aliases: ['b', 'bos'],
 	help: help.get('commands_general', 'boss').value,
-	/*
-	actions: [
-		{
-			name: 'DEFAULT',
-			//aliases: [],
-			synopsis: 'Show current bosses.'
-		},
-		{
-		name: 'add',
-		aliases: [],
-		synopsis: 'Add a boss to the bosses list.'
-		},
-		{
-		name: 'list',
-		aliases: ['ls'],
-		synopsis: 'List current bosses.'
-		},
-		{
-		name: 'remove',
-		aliases: ['rem', 'rm'],
-		synopsis: 'Remove a boss from the bosses list.'
-		},
-		{
-		name: 'update',
-		aliases: [],
-		synopsis: 'Update the bosses list.'
-		},
-	],
-	*/
 	actions_admin: ['add', 'remove', 'update'],
-	//actions_list: ['add', 'perfect', 'remove', 'type', 'update'],
-	//synopsis: 'Show bosses.',
-	//description: 'Display current Raid Bosses',
-	//syntax: ['boss {boss name}'],
-	/*
-	usage: {
-		'To show bosses:': 'boss',
-		'To show specific boss:': 'boss darkari',
-		'To show a tier': 'boss l5'
-	},
-	*/
 	cooldown: 5,
 	execute(message, argv) {
-	
-		console.log('ENTERING MAIN !!BOSS COMMAND!!!')
-		console.log('ARGV: ', argv)
-		console.log('HELP?????', this.help)
 		
 		let args = argv._
-		let action = null
-	
-	/**
-		ERROR: Need to run logic for when multiple bosses are found. For example, "alo" will only find the first match, obviously.
-	**/
 
 		const embed = new Discord.MessageEmbed()
 		embed.setColor(colors.primary)
+
+		// ACTIONS: VALUES MUST MATCH FILE NAMES IN /BOSS_ACTIONS
+		let actions = [
+			{name: 'add'},
+			{name: 'list', aliases: ['ls']},
+			{name: 'remove', aliases: ['rem', 'rm']},
+			{name: 'update'},
+		]
+
+		// SET DEFAULT ACTION IF NEEDED
+		if(!args.length) args.push('list')
+
+		let action = (
+			actions.find(action => action.name == args[0]) || 
+			actions.find(action => action.aliases && action.aliases.includes(args[0])) ||
+			null
+		)
 
 		// EXIT EARLY IF ADMIN ACTION ATTEMPT BY NON-ADMIN.
 		if(this.actions_admin.includes(args[0]) && !discord.isAdmin(message)){			
@@ -90,30 +56,7 @@ module.exports = {
 			message.channel.send(embed)
 			return
 		}
-		
-		// ACTION: ADD
-		if(args[0] === 'add'){
-			action = 'add'
-			args.shift()
-		}
-		
-		// ACTION: LIST (DEFAULT)
-		if((args.length == 0) || args[0] === 'list' || args[0] === 'ls'){
-			action = 'list'
-			args.shift()
-		}
-		
-		// ACTION: REMOVE
-		if(args[0] === 'remove' || args[0] === 'rem' || args[0] === 'rm'){
-			action = 'remove'
-			args.shift()
-		}
-	
-		// ACTION: UPDATE
-		if(args[0] === 'update'){
-			action = 'update'
-		}
-		
+
 		// SHOW TIER
 		let detect_boss_tier = detect.boss_tier(args)
 		if(!action && !detect_boss_tier.error){
@@ -123,17 +66,17 @@ module.exports = {
 			message.channel.send(embed)
 			return
 		}
-		
+
 		// SHOW SPECIFIC BOSS
-		if(!action && args.length) {
+		if(!action) {
 			console.log(args)
-			action = 'show_boss'
+			action = {name: 'show_boss'}
 		}
-		
+
 		// RUN ACTION
 		if(action) {
 			var data = {embed, args, message}
-			require(`./boss_actions/${action}`)(data)
+			require(`./boss_actions/${action.name}`)(data)
 			return
 		}
 
--- discord/commands_general/commands/boss_actions/show_boss.js
+++ discord/commands_general/commands/boss_actions/show_boss.js
@@ -4,19 +4,40 @@ const {colors} = require(`@config`).discord
 module.exports = ({embed, args, message}) => {
 
 	let filter = null
+	
+	let filters = [
+		{name: 'charge', aliases: ['cha', 'chrg']},
+		{name: 'fast', aliases: ['fas', 'fst']},
+		{name: 'perfect', aliases: ['per', 100]},
+		{name: 'type', aliases: ['typ', 'tp']},
+	]
+	
+	// FIND OPTIONAL FILTER AND REMOVE FROM ARGS.
+	for (let step = 0; step < args.length; step++) {
 
-	if(['charge', 'fast', 'perfect', 'type'].includes(args[0])){
-		filter = args[0]
-		args.shift()
+		let arg = args[step]
+		
+		filter = (
+			filters.find(filter => filter.name == arg) || 
+			filters.find(filter => filter.aliases && filter.aliases.includes(arg)) ||
+			{name: null}
+		).name
+		
+		if(filter){
+			args = args.filter(item => item !== arg)
+			break
+		}
 	}
 
 	// DETECT BOSS
 	let detectBoss = detect.boss(args)
 
 	if(detectBoss.error){
+		console.log(detectBoss)
 		embed.setColor(colors.error)
 		embed.setTitle('Boss Error')
 		embed.addField('Error', detectBoss.error_message)
+		embed.addField('Found', detectBoss.value)
 		message.channel.send(embed)
 		return
 	}
