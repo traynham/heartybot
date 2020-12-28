9545de27b79491bb52d361c0dc8145afff546bef
traynham <jesse@comm-unity.net>
2020-12-28 23:45:31 +0000

    Cleaed up code.

--- core/discord/embedCommand.js
+++ core/discord/embedCommand.js
@@ -23,21 +23,19 @@ const payload_obj = require('@core/util/payload')
 
 module.exports = (message, args, command_set) => {
 
-//console.log(args)
-
 	const {util} = require(`@core`)
-		
+
 		let payload = payload_obj()
 
 		let commands = message.client[command_set]
 		let help = {}
-		
+
 		let user_roles = Array.from(message.member.roles.cache.values()).map(role => role.name.toLowerCase())
-		
+
 		const embed = new Discord.MessageEmbed()
 		embed.setColor(colors.primary)
 		embed.setThumbnail(util.emoji_img('book', {h: 25}).value)
-		
+
 		payload.value = embed
 
 		const help_footer =	'__________\n' +
@@ -45,7 +43,7 @@ module.exports = (message, args, command_set) => {
 									'Items in {curly brackets} are optional.'
 
 		const commandName = args[0]
-		
+
 		const command = (
 			commands.get(commandName) || 
 			commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName)) ||
@@ -54,7 +52,7 @@ module.exports = (message, args, command_set) => {
 
 		//if(command) help = command.help
 		if(command) help = command.meta
-		
+
 		// IF COMMAND IS NOT FOUND, BUT REQUESTED.
 		if(args.length > 0 && !command){
 			payload.error = true
@@ -69,12 +67,12 @@ module.exports = (message, args, command_set) => {
 		if(args.length > 1 && help){
 
 			let requested_action = args[1]
-			
+
 			let action = help.actions.find(action =>
 					action.name == requested_action || 
 					(action.aliases && action.aliases.includes(requested_action))
 			)
-			
+
 			if(!action){
 				payload.error = true
 				payload.error_message = `Sorry. That action was not found.`
@@ -83,14 +81,14 @@ module.exports = (message, args, command_set) => {
 				embed.setDescription(payload.error_message)
 				return payload
 			}
-			
+
 			embed.setTitle(`**HELP › ${help.name} › ${action.name}**`)
 			embed.setDescription(`${action.description}\n${emoji.spacer}`)
-			
+
 			if(action.aliases) {
 				embed.addField('**Aliases**', action.aliases.join(', ') + `\n${emoji.spacer}`)
 			}
-			
+
 			if(action.syntax){
 				embed.addField('**Sytax**', '`' + action.syntax + '`' + `\n${emoji.spacer}`)
 			}
@@ -101,7 +99,7 @@ module.exports = (message, args, command_set) => {
 				})	
 				embed.addField('**Examples**', examples)
 			}
-			
+
 			if(help.show_help_footer){embed.setFooter(help_footer)}
 
 			return payload
@@ -116,22 +114,21 @@ module.exports = (message, args, command_set) => {
 			if(help.aliases.length > 0) embed.addField('**Aliases**', help.aliases.join(', ') + `\n${emoji.spacer}\n`)
 
 			if(help.actions.length > 0) {
-				
+
 				let actions = []
-				
+
 				help.actions.forEach(action => {
 					if(action.roles && !user_roles.includes(action.roles[0])) { return }
 					actions.push(action.name + (action.default ? ' (Default)' : ''))
 				})
-				
+
 				embed.addField(
 					'**Actions**', 
 					actions.join(', ') + '\n\nUse `?' + help.name + ' [action]` for addtional help.\n' + emoji.spacer
 				)
 
 			}
-			
-			//if(help.syntax) embed.addField('**Syntax**', help.syntax.join('\n') + `\n${emoji.spacer}`)
+
 			if(help.syntax) embed.addField('**Syntax**', '`' + help.syntax + '`' + `\n${emoji.spacer}`)
 
 			if(help.usage){
