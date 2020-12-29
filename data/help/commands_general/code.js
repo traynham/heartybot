f8047f27ec022f7c3575314bbbd7b3a51f37154a
traynham <jesse@comm-unity.net>
2020-12-29 00:41:44 +0000

    Updated with newer help system.

--- data/help/commands_general/code.js
+++ data/help/commands_general/code.js
@@ -3,53 +3,54 @@ const {prefix: p, syntax_optional: so, syntax_required: sr} = require('@config')
 module.exports = {
 
 	// GENERAL
-	name:						'code',
+	name:							'code',
 	synopsis:					'View a trainer code',
-	description:				'This command allows you to show your own code or view other player codes.',
+	description:				`This command allows you to show your own code or view other player codes.\n\n To set your code, see the ${p}me command`,
 	syntax:						`${p}code ${so[0]}@mention|code${so[1]}`,
-	alias:						[],
+	aliases:						['cd'],
 	dm:							true,
-	availability:				['PogoNWFortWorth', 'Pogo Barn'],
-	revision:					'08-21-20',
-
-
-	//DEFAULT ACTION
-	default: {
-		description_short:		'Returns trainer code with QRCode card',
-		synopsis:				'Returns trainer code with QRCode card',
-		description:			'This command will display your trainer code along with a QRCode for easy scanning.',
-		examples:				[
-									{code: `${p}code`}
-								]
-	},
+	revision:					'12-28-20',
 
 	// ACTIONS
-	actions: {
+	actions: [
+
+		// Code
+		{
+			name:					'code',
+			synopsis:			'Display a code card by passing a trainer code.',
+			description:		`This action allows you to display a code card for a trainer code, even if it is not in the system.`,
+			examples: 			{
+										'Show code card for "1234 5678 9012".': `${p}code 1234 5678 9012`,
+										'Show code card for "123456789012".': `${p}code 123456789012`
+									},
+			required:			true,
+			syntax:				`${p}code ${sr[0]}code${sr[1]}`
+		},
 
 		// Mention
-		mention: {
-			name:				'@mention',
+		{
+			name:					'@mention',
 			synopsis:			'View a code card by mentioning a discord member.',
 			syntax:				`${p}code ${sr[0]}@mention${sr[1]}`,
 			description:		'This action allows you to look up a trainer code and qrcode by mentioning them.',
-			examples:			[
-									{code: `${p}code @heartyjessman`, description: "Show heartyjessman's code card."}
-								]
-		},
+			examples:			{
+										"Show heartyjessman's code card.": `${p}code @heartyjessman`
+									}
 
-		// Code
-		code: {
-			synopsis:			'Display a code card by passing a trainer code.',
-			description:		`This action allows you to display a code card for a trainer code, even if it is not in the system.`,
-			examples: 			[
-									{code: `${p}code 1234 5678 9012`, description: 'Show code card for "1234 5678 9012".'},
-									{code: `${p}code 123456789012`, description: 'Show code card for "123456789012".'}
-								],
-			name:				'Code',
-			required:			true,
-			syntax:				`${p}code ${sr[0]}code${sr[1]}`
+		},
+		
+		// SHOW (CURRENTLY A BOGUS COMMAND THAT WORKS BY DEFAULT)
+		{
+			name:					'show',
+			default:				true,
+			synopsis:			'View your trainer code card.',
+			description:		'This action allows you to show your trainer code card.',
+			syntax:				`${p}code ${sr[0]}show${sr[1]}`,
+			examples:			{
+										'Show your card': `${p}code show`
+									}
 		}
 
-	} // ACTIONS
+	] // ACTIONS
 
 }
--- discord/commands_general/commands/code.js
+++ discord/commands_general/commands/code.js
@@ -1,103 +1,109 @@
-const Discord = require('discord.js');
-//const QRCode = require('qrcode')
+/**
+ * @module Code
+ * @author Jesse Traynham
+ * @category Discord Commands
+ * @subcategory General
+ */
 
-const {me, qr, detect} = require(`@core`)
+/**
+ * @param {object} message Discord message
+ * @param {array} argv Arguments array from yargs.
+ * @function
+ * @name code
+ */
+
+const Discord = require('discord.js');
+
+const {detect, help, me, qr} = require(`@core`)
 const {colors} = require(`@config`).discord
 
 module.exports = {
 	name: 'code',
-	aliases: [],
-	description: 'Pogo Trainer Code',
+	meta: help.get('commands_general', 'code').value,
 	cooldown: 5,
-	//execute(message, args) {
 	async execute(message, argv) {
 	
-	
-//		(async () => {
 
-			let args = argv._
-			
-			// SEE IF ARGS IS A CODE
-			let detectCode = detect.code(args)
-			let code = !detectCode.error ? detectCode.value_formatted : null
-
-			let content = null
-			let description = null
-
-			// SET USER TO FIRST MENTION OR THE AUTHOR RESPECTIVELY.
-			let user = message.mentions.users.first() || message.author 
-
-			// GRAB MEMBER RECORD FROM DB.
-			let member = await me.find(user.id)
-			
-			// TRY TO FIND MEMBER BY THEIR CODE			
-//			if(code) member = await me.find({trainer_code: code})
-			if(code) member = await me.find({trainer_code: detectCode.value})
-			
-			// MESSAGE AUTHOR ID AND USER ID ARE THE SAME?
-			let isOwner = user.id === message.author.id
-
-			// CREATE EMBED
-			const embed = new Discord.MessageEmbed()
-			embed.setColor(colors.code)
-
-			if(member){
-
-				let hide_code = member.privacy.split(',').includes('code')
-				// HONOR HIDE_CODE, BUT ALLOW OWNER TO POST CODE.
-				if(hide_code && !isOwner){
-					description = `Sorry, <@${user.id}>'s code is set to private.`
-				}
-				
-				// ISOWNER AND NO CODE SET
-				else if(!member.trainer_code && isOwner && !detectCode.error){
-					description = `Sorry, <@${user.id}>, you have not set your code yet. See help for more information.`
-				}
-				
-				// NO CODE SET
-				else if(!member.trainer_code && !detectCode.error){
-					description = `Sorry, ${user.username} has not set their code.`
-				}
+		let args = argv._
 		
-				// POST CARD.
-				else if(member.trainer_code){
+		// SEE IF ARGS IS A CODE
+		let detectCode = detect.code(args)
+		let code = !detectCode.error ? detectCode.value_formatted : null
 
-					if(code){
-						embed.setAuthor(`${member.nickname}'s trainer code`)
-					} else {
-						embed.setAuthor(`${member.trainer_name}'s trainer code`, user.displayAvatarURL())
-					}
-					embed.addField("Here's my friend code:", member.trainer_code)
-					detectCode = detect.code(member.trainer_code)
-					code = detectCode.value
-				}
+		let content = null
+		let description = null
+
+		// SET USER TO FIRST MENTION OR THE AUTHOR RESPECTIVELY.
+		let user = message.mentions.users.first() || message.author 
+
+		// GRAB MEMBER RECORD FROM DB.
+		let member = await me.find(user.id)
 		
+		// TRY TO FIND MEMBER BY THEIR CODE			
+		if(code) member = await me.find({trainer_code: detectCode.value})
+		
+		// MESSAGE AUTHOR ID AND USER ID ARE THE SAME?
+		let isOwner = user.id === message.author.id
+
+		// CREATE EMBED
+		const embed = new Discord.MessageEmbed()
+		embed.setColor(colors.code)
+
+		if(member){
+
+			let hide_code = member.privacy.split(',').includes('code')
+			// HONOR HIDE_CODE, BUT ALLOW OWNER TO POST CODE.
+			if(hide_code && !isOwner){
+				description = `Sorry, <@${user.id}>'s code is set to private.`
 			}
-
-			// ADD DESCRIPTION IF NEEDED
-			if(description) embed.setDescription(description)
-
-			if(code){
-
-				let trainer_qr = await qr.create({
-					name: `${detectCode.value}.png`,
-					value: detectCode.value,
-				})
-	
-				const attachment = new Discord.MessageAttachment(trainer_qr.path, trainer_qr.name);
-
-				embed.attachFiles(attachment)
-				embed.setImage(`attachment://${trainer_qr.name}`);
-				embed.setFooter('Note: long press on mobile to copy code as text.')
-				
-				content = detectCode.value_formatted
 			
-			} // IF CODE
+			// ISOWNER AND NO CODE SET
+			else if(!member.trainer_code && isOwner && !detectCode.error){
+				description = `Sorry, <@${user.id}>, you have not set your code yet. See help for more information.`
+			}
+			
+			// NO CODE SET
+			else if(!member.trainer_code && !detectCode.error){
+				description = `Sorry, ${user.username} has not set their code.`
+			}
+	
+			// POST CARD.
+			else if(member.trainer_code){
 
-			// SEND MESSAGE
-			message.channel.send(content, embed);
+				if(code){
+					embed.setAuthor(`${member.nickname}'s trainer code`)
+				} else {
+					embed.setAuthor(`${member.trainer_name}'s trainer code`, user.displayAvatarURL())
+				}
+				embed.addField("Here's my friend code:", member.trainer_code)
+				detectCode = detect.code(member.trainer_code)
+				code = detectCode.value
+			}
+	
+		}
 
-//		})() // ASYNC
+		// ADD DESCRIPTION IF NEEDED
+		if(description) embed.setDescription(description)
+
+		if(code){
+
+			let trainer_qr = await qr.create({
+				name: `${detectCode.value}.png`,
+				value: detectCode.value,
+			})
+
+			const attachment = new Discord.MessageAttachment(trainer_qr.path, trainer_qr.name);
+
+			embed.attachFiles(attachment)
+			embed.setImage(`attachment://${trainer_qr.name}`);
+			embed.setFooter('Note: long press on mobile to copy code as text.')
+			
+			content = detectCode.value_formatted
+		
+		} // IF CODE
+
+		// SEND MESSAGE
+		message.channel.send(content, embed);
 
 	} // EXECUTE
 
