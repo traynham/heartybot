520f16a4db9ecc0ff113925cbb20456b3530ab66
traynham <jesse@comm-unity.net>
2021-01-12 23:31:57 +0000

    Cleaned up code.

--- discord/commands_general/commands/types.js
+++ discord/commands_general/commands/types.js
@@ -7,95 +7,82 @@ const config = require(`@config`)
 module.exports = {
 	name: 'types',
 	meta: help.get('commands_general', 'types').value,
-	//aliases: ['type', 't'],
-	//description: 'Get Pokemon Type Infos',
 	cooldown: 5,
-//	execute(message, args) {
 	async execute(message, argv) {
 	
-//		(async () => {
+		let args = argv._
+
+		let type_images = config.general.pokemon.type_images
+
+		// CREATE EMBED
+		const embed = new Discord.MessageEmbed()
+		embed.setColor(colors.primary)	
+
+		// POST INFOGRAPHIC		
+		if(args.length == 0){
+
+			embed.setTitle('**Pokémon Types**')
+			embed.setURL(`${config.main.domain}/infographics/types`)
 		
-			let args = argv._
+			discord.setImage(embed, 'public/images/infographics/types.png', 'types.png')
 
-			let type_images = config.general.pokemon.type_images
+		}
 
-			// CREATE EMBED
-			const embed = new Discord.MessageEmbed()
-			embed.setColor(colors.primary)	
+		// POST LISTING
+		if(args.join('') == 'list'){
 
-			// POST INFOGRAPHIC		
-			if(args.length == 0){
+			let types = pokedex.type('all')
 
-				embed.setTitle('**Pokémon Types**')
-				embed.setURL(`${config.main.domain}/infographics/types`)
+			embed.setTitle(`**Pokémon Go Types**`)
+			embed.setURL(`${config.main.domain}/infographics/types`)
+			embed.setDescription(
+				types.rows.map( type => type.emoji + ' \u200B ' + util.titleCase(type.name) )
+			)
+
+		}
+
+		// POST TYPE INFO
+		if(args.length && !(args.join('') == 'list')){
+		
+			let type = pokedex.type(args)
+
+			if(type.error){
 			
-				discord.setImage(embed, 'public/images/infographics/types.png', 'types.png')
-
-			}
-
-			// POST LISTING
-			if(args.join('') == 'list'){
-								
 				let types = pokedex.type('all')
 
-				embed.setTitle(`**Pokémon Go Types**`)
-				embed.setURL(`${config.main.domain}/infographics/types`)
+				embed.setTitle(`**Error...**`)
+				embed.setColor(colors.error)
+
 				embed.setDescription(
-					//types.values.map( type => type.emoji + ' \u200B ' + util.titleCase(type.name) )
-					types.rows.map( type => type.emoji + ' \u200B ' + util.titleCase(type.name) )
+					'Type not found. Use a type from blow: ' + '\n\u200B\n\u200B' +
+					types.rows.map( type => type.emoji + ' \u200B ' + util.titleCase(type.name) ).join('\n')
 				)
 
-			}
+			} // IF ERROR
 
-			// POST TYPE INFO
-			if(args.length && !(args.join('') == 'list')){
+			if(!type.error){
+
+				let items = ['super_effective', 'not_effective', 'resistant_to', 'weak_to']
+
+				embed.setTitle(`**Type: ${util.titleCase(type.value.name)}**`)
+				embed.setURL(`${config.main.domain}/infographics/types/${type.value.name}`)
 			
-				let type = pokedex.type(args)
-
-				if(type.error){
-				
-					let types = pokedex.type('all')
-
-					embed.setTitle(`**Error...**`)
-					embed.setColor(colors.error)
-					/*
-					embed.setDescription(
-						'Type not found. Use a type from blow: ' + '\n\u200B\n\u200B' +
-						types.values.map( type => type.emoji + ' \u200B ' + util.titleCase(type.name) ).join('\n')
+				items.forEach(function(item){
+					embed.addField(
+						util.titleCase(item),
+						type.value[item].toLowerCase().split(', ').map( type => emoji[type] ).join(' ') + '\n\u200B'
 					)
-					*/
-					
-					embed.setDescription(
-						'Type not found. Use a type from blow: ' + '\n\u200B\n\u200B' +
-						types.rows.map( type => type.emoji + ' \u200B ' + util.titleCase(type.name) ).join('\n')
-					)
+				})
 
-				} // IF ERROR
-				
-				if(!type.error){
-					
-					let items = ['super_effective', 'not_effective', 'resistant_to', 'weak_to']
+				embed.setThumbnail(`https://images.weserv.nl/?h=96&il&url=${type_images[type.value.name]}`)
 
-					embed.setTitle(`**Type: ${util.titleCase(type.value.name)}**`)
-					embed.setURL(`${config.main.domain}/infographics/types/${type.value.name}`)
-				
-					items.forEach(function(item){
-						embed.addField(
-							util.titleCase(item),
-							type.value[item].toLowerCase().split(', ').map( type => emoji[type] ).join(' ') + '\n\u200B'
-						)
-					})
-
-					embed.setThumbnail(`https://images.weserv.nl/?h=96&il&url=${type_images[type.value.name]}`)
-					
-				}
-				
 			}
-
-			// SEND MESSAGE
-			message.channel.send(embed)
 			
-//		})() // ASYNC
+		}
+
+		// SEND MESSAGE
+		message.channel.send(embed)
+
 	} // EXECUTE
 
 } // MODULE.EXPORTS
