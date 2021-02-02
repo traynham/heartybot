2fbe97f4c61105b54696964f285d8b5426045b69
traynham <jesse@comm-unity.net>
2021-02-02 22:35:52 +0000

    Actually wrote the started command code.

--- discord/commands_general/commands/me_actions/started.js
+++ discord/commands_general/commands/me_actions/started.js
@@ -1,8 +1,25 @@
-/**
-	@todo Format date
-*/
-module.exports = ({embed, member}) => {
+const dateFormat = require('dateformat')
+const {colors} = require('@config').discord
+const {me} = require('@core')
 
-	embed.addField('Started', member.started)
+module.exports = async ({embed, member, value}) => {
+
+	if(value){
+
+		const started = new Date(value)
+
+		let update = await me.set(member.id, {started: started})
+
+		if(update){
+			embed.setColor(colors.success)
+			embed.setDescription(`<@${member.snowflake}> your start date was set.`)
+			embed.addField('Started', dateFormat(started, "m/dd/yy"))
+		}
+
+		return true
+
+	}
+
+	embed.addField('Started', dateFormat(member.started, "m/dd/yy"))
 
 }
