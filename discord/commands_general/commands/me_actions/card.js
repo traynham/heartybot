bcf2ba23d2318be752f79d8422372fcddec43c5f
traynham <jesse@comm-unity.net>
2021-02-02 22:38:05 +0000

    Formatted started date using dateFormat.

--- discord/commands_general/commands/me_actions/card.js
+++ discord/commands_general/commands/me_actions/card.js
@@ -1,3 +1,5 @@
+const dateFormat = require('dateformat')
+
 module.exports = async ({embed, member, message}) => {
 
 	let privacy = member.privacy.split(',')
@@ -5,7 +7,7 @@ module.exports = async ({embed, member, message}) => {
 	if(!privacy.includes('name')) embed.addField('Name', member.nickname)
 	if(!privacy.includes('team')) embed.addField('Team', member.team)
 	if(!privacy.includes('level')) embed.addField('Level', member.level)
-	if(!privacy.includes('started')) embed.addField('Started', member.started)
+	if(!privacy.includes('started')) embed.addField('Started', dateFormat(member.started, "m/dd/yy"))
 	if(!privacy.includes('code') && member.trainer_code) embed.addField('Friend Code', member.trainer_code)
 	embed.setThumbnail(message.author.displayAvatarURL())
 
