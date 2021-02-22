99beac9ded1fbe0c5e973162de1ca0d34aefceab
traynham <jesse@comm-unity.net>
2021-02-22 23:48:28 +0000

    initial commit.

---
+++ bin/setup.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+require('dotenv').config({ path: '../config/.env' })
+/*
+
+const moduleAlias = require('module-alias')
+moduleAlias(__dirname + '/../package.json')
+
+*/
+
+require('module-alias/register')
+
+const config = require('@config')
+
+
+console.log(config)
+console.log('Hearty Setup')
